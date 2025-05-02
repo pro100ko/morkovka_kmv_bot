a911759e1a7b057bd3f8e5494a25f9e2fa3adb06
pro100ko <111150165+pro100ko@users.noreply.github.com>
2025-05-02 11:27:35 +0300

    Delete morkovka_kmv_bot directory

--- morkovka_kmv_bot/utils/search_utils.py
+++
@@ -1,9 +0,0 @@
-from firebase_db import FirebaseDB
-from config import Config
-
-
-def search_products(query):
-    if len(query) < Config.MIN_SEARCH_QUERY_LENGTH:
-        return []
-
-    return FirebaseDB.search_products(query)[:Config.MAX_SEARCH_RESULTS]
